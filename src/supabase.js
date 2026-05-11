bd764ce637d2c37ea3ffe5a4faf6b21ab37f8205
andrecoba-alt <andrecoba@oloverstudio.com>
2026-05-11 08:02:16 -0500

    Create supabase.js

---
+++ src/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const SUPABASE_URL = 'https://gjrujyfhttnacjcljsje.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_lJeZ8nCl0L-FGTyWbWzAYw_cOv-02Zb';
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
