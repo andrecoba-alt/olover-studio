8fefee0bce9af16e41fc7e29d01a0e6daec2432f
andrecoba-alt <andrecoba@oloverstudio.com>
2026-05-11 14:47:59 -0500

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const SUPABASE_URL = 'https://gjrujyfhttnacjcljsje.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_lJeZ8nCl0L-FGTyWbWzAYw_cOv-02Zb';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqcnVqeWZodHRuYWNqY2xqc2plIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0OTk5MDMsImV4cCI6MjA5NDA3NTkwM30.tw0LsLwNbMHcB6qzSuzl35OkF_RS8KZxTf4Zh5XCv1U';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
-
-export const REDIRECT_URL = 'https://olover-studio.vercel.app';
